99611e99407deaa5936feb3f604e61628d69051b
Fernando <fernando.lima@ccc.ufcg.edu.br>
2023-06-06 20:32:06 -0300

    ProductShelfAndCard

--- live.gen.ts
+++ live.gen.ts
@@ -30,9 +30,10 @@ import * as $$$$$$$$9 from "./sections/Highlights.tsx";
 import * as $$$$$$$$10 from "./sections/LinkTree.tsx";
 import * as $$$$$$$$11 from "./sections/ProductDetails.tsx";
 import * as $$$$$$$$12 from "./sections/ProductShelf.tsx";
-import * as $$$$$$$$13 from "./sections/SearchResult.tsx";
-import * as $$$$$$$$14 from "./sections/WhatsApp.tsx";
-import * as $$$$$$$$15 from "./sections/WishlistGallery.tsx";
+import * as $$$$$$$$13 from "./sections/ProductShelfAndCard.tsx";
+import * as $$$$$$$$14 from "./sections/SearchResult.tsx";
+import * as $$$$$$$$15 from "./sections/WhatsApp.tsx";
+import * as $$$$$$$$16 from "./sections/WishlistGallery.tsx";
 import * as $live_middleware from "$live/routes/_middleware.ts";
 import * as $live_workbench from "$live/routes/live/workbench.ts";
 import * as $live_invoke from "$live/routes/live/invoke/index.ts";
@@ -202,9 +203,10 @@ const manifest = {
     "deco-sites/suztec/sections/LinkTree.tsx": $$$$$$$$10,
     "deco-sites/suztec/sections/ProductDetails.tsx": $$$$$$$$11,
     "deco-sites/suztec/sections/ProductShelf.tsx": $$$$$$$$12,
-    "deco-sites/suztec/sections/SearchResult.tsx": $$$$$$$$13,
-    "deco-sites/suztec/sections/WhatsApp.tsx": $$$$$$$$14,
-    "deco-sites/suztec/sections/WishlistGallery.tsx": $$$$$$$$15,
+    "deco-sites/suztec/sections/ProductShelfAndCard.tsx": $$$$$$$$13,
+    "deco-sites/suztec/sections/SearchResult.tsx": $$$$$$$$14,
+    "deco-sites/suztec/sections/WhatsApp.tsx": $$$$$$$$15,
+    "deco-sites/suztec/sections/WishlistGallery.tsx": $$$$$$$$16,
   },
   "loaders": {
     "$live/loaders/state.ts": i2$$$0,
